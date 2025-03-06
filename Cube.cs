7d14b0b6bc2ac14ba4f291d6760a0e7dec00c1c8
Emma Cliff <Emma@DN-UK.COM>
2025-03-06 09:45:46 +0000

    Add project files.

---
+++ Cube.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotatingCube;
+
+public class Pt
+{
+    public double x { get; set; }
+    public double y { get; set; }
+    public double z { get; set; }
+
+    public Pt(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+public class Cube
+{
+    public Pt position;
+    private double scale;
+    private List<Pt> model = new List<Pt>();
+    private Form1 form;
+
+    private Pen pen = new Pen(Color.Black, 5f);
+
+    public Cube(Form1 f, int x, int y, int z, int scale)
+    {
+        form = f;
+        // Create cube at a random position
+        Random r = new Random();
+        x = r.Next(-form.ClientSize.Width / 2, form.ClientSize.Width / 2);
+        y = r.Next(-form.ClientSize.Height / 2, form.ClientSize.Height / 2);
+        z = r.Next(0, form.ClientSize.Height);
+
+        position = new Pt(x, y, z);
+        this.scale = scale;
+
+        model.Add(new Pt(-1, 1, -1));
+        model.Add(new Pt(1, 1, -1));
+        model.Add(new Pt(1, -1, -1));
+        model.Add(new Pt(-1, -1, -1));
+    }
+
+    public void Draw(Graphics g)
+    {
+        List<Pt> points = new List<Pt>();
+
+        foreach (Pt p in model)
+        {
+            points.Add(new Pt((int)(position.x + p.x * scale), (int)(position.y + p.y * scale), (int)(position.z + p.z * scale)));
+        }
+
+        g.DrawLine(pen, (int)points[0].x, (int)points[0].y, (int)points[1].x, (int)points[1].y);
+        g.DrawLine(pen, (int)points[1].x, (int)points[1].y, (int)points[2].x, (int)points[2].y);
+        g.DrawLine(pen, (int)points[2].x, (int)points[2].y, (int)points[3].x, (int)points[3].y);
+        g.DrawLine(pen, (int)points[3].x, (int)points[3].y, (int)points[0].x, (int)points[0].y);
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace RotatingCube
+{
+    public partial class Form1 : Form
+    {
+        private Pen pen = new Pen(Color.Black, 5f);
+        private Cube[] cubes = new Cube[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < cubes.Length; i++)
+            {
+                cubes[i] = new Cube(this, 200, 10, 20, 100);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.TranslateTransform(ClientSize.Width / 2, ClientSize.Height / 2);
+            g.ScaleTransform(1, -1);
+
+            cubes[0].position.x += 1;
+
+            foreach (Cube cube in cubes) cube.Draw(g);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
